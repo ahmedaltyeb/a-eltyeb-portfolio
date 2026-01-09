79f35bbfb4f691f7f04576520d8b25e1cb162bdb
@ahmed_altyeb <67749498+ahmedaltyeb@users.noreply.github.com>
2026-01-09 19:20:25 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/a-eltyeb-portfolio/",   // أضف هذا السطر
-  server: { 
+export default defineConfig(({ mode }) => ({  
+  // هذا السطر يكتشف إذا كان الموقع مرفوعاً على GitHub Pages أم Netlify تلقائياً
+  base: process.env.NETLIFY || process.env.VERCEL ? "/" : "/a-eltyeb-portfolio/",
+  
+  server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
