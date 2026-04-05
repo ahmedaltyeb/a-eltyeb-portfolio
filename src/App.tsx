343676bbaf5475b48f6cf2b3c391a8a0088491a9
@ahmed_altyeb <67749498+ahmedaltyeb@users.noreply.github.com>
2026-04-05 17:19:52 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,19 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-// تحديد الـ basename ديناميكياً
-// إذا كان الرابط يحتوي على github.io سيستخدم اسم المستودع، وإلا سيستخدم المسار الرئيسي
-const basename = window.location.hostname.includes("github.io") 
-  ? "/a-eltyeb-portfolio" 
-  : "/";
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter basename={basename}>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+    <BrowserRouter basename="/a-eltyeb-portfolio">
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+  </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
 );
 
 export default App;
